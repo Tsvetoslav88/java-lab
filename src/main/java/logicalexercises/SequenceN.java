25a4c8b0069299c52961929f50612a18de2fdffc
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2018-02-11 13:19:28 +0200

    Added a new logical exercise

---
+++ src/main/java/logicalexercises/SequenceN.java
@@ -0,0 +1,32 @@
+package logicalexercises;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * S = N, N+1, 2*N, N+2, 2*(N+1), 2*N+1, N*4,...
+ * 
+ * @author TSVETOSLAV
+ *
+ */
+public class SequenceN {
+
+	public static void main(String[] args) {
+		int n = 3;
+		int p = 5;
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(n);
+		int index = 0;
+		while (queue.size() > 0) {
+			int current = queue.remove();
+			index++;
+			if (current == p) {
+				System.out.println("Found at index " + index);
+				break;
+			}
+			queue.add(current + 1);
+			queue.add(2 * current);
+		}
+	}
+
+}
