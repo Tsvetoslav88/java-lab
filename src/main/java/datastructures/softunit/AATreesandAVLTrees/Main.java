e65ce418867aade36b5f0883d2e076bf6b4a3a6a
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2018-02-18 22:14:59 +0200

    AVLTree implemented

---
+++ src/main/java/datastructures/softunit/AATreesandAVLTrees/Main.java
@@ -0,0 +1,14 @@
+package datastructures.softunit.AATreesandAVLTrees;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AVL<Integer> tree = new AVL<>();
+		tree.insert(4);
+		tree.insert(2);
+		tree.insert(3);
+		// tree.insert(4);
+		// tree.insert(5);
+		// tree.insert(6);
+	}
+}
