8c48ed5792e8c1f73440b3de7bfd75edb0449b32
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2018-03-12 21:07:00 +0200

    Added Predicate function

--- src/main/java/oraclecertifications/oca/lambda/TraditionalSearch.java
+++ src/main/java/oraclecertifications/oca/lambda/TraditionalSearch.java
@@ -2,6 +2,7 @@ package oraclecertifications.oca.lambda;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class TraditionalSearch {
 	public static void main(String[] args) {
@@ -11,20 +12,30 @@ public class TraditionalSearch {
 		animals.add(new Animal("rabbit", true, false));
 		animals.add(new Animal("turtle", false, true));
 
-		//Java 7 and older style
-//		print(animals, new CheckIfHopper()); // pass class that does check
-		//Java 7 lambda expression
+		// Java 7 and older style
+		// print(animals, new CheckIfHopper()); // pass class that does check
+		// Java 7 lambda expression
 		print(animals, a -> a.canHop());
 		print(animals, a -> a.canSwim());
-		
+
 	}
 
-	private static void print(List<Animal> animals, CheckTrait checker) {
+	// Using our own interface
+	// private static void print(List<Animal> animals, CheckTrait checker) {
+	// for (Animal animal : animals) {
+	// if (checker.test(animal)) // the general check
+	// System.out.print(animal + " ");
+	// }
+	// System.out.println();
+	// }
+
+	// Using Predicate interface
+	private static void print(List<Animal> animals, Predicate<Animal> checker) {
 		for (Animal animal : animals) {
 			if (checker.test(animal)) // the general check
 				System.out.print(animal + " ");
 		}
 		System.out.println();
 	}
-	
+
 }
