57551c699a9a33e6dad6637f59141ce77381cba7
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2019-01-07 21:06:06 +0200

    Implement BinaryTree Traversals(Inorder, Preorder, Postorder)

--- src/main/java/crackingthecodinginterview/treesandgraphs/BinaryTreeExample.java
+++ src/main/java/crackingthecodinginterview/treesandgraphs/BinaryTreeExample.java
@@ -17,42 +17,70 @@ class BinaryTree {
 		this.root = null;
 	}
 
-	// Wrappers over above recursive functions
-	void printPostorder() {
-		printPostorder(this.root);
+	/*
+	 * Given a binary tree, print its nodes according to the "bottom-up"
+	 * postorder traversal.
+	 */
+	private void printPostorder(Node root2) {
+		if (root2 == null) {
+			return;
+		}
+
+		// first recur on left subtree
+		printPostorder(root2.left);
+
+		// then recur on right subtree
+		printPostorder(root2.right);
+
+		// now deal with the node
+		System.out.print(root2.key + " ");
+
 	}
 
-	private void printPostorder(Node root2) {
-		// TODO Auto-generated method stub
-		
+	/* Given a binary tree, print its nodes in inorder */
+	private void printInorder(Node root2) {
+		if (root2 == null) {
+			return;
+		}
+
+		/* first recur on left child */
+		printInorder(root2.left);
+
+		/* then print the data of node */
+		System.out.print(root2.key + " ");
+
+		/* now recur on right child */
+		printInorder(root2.right);
+
+	}
+
+	/* Given a binary tree, print its nodes in preorder */
+	private void printPreorder(Node root2) {
+		if (root2 == null) {
+			return;
+		}
+
+		/* first print data of node */
+		System.out.print(root2.key + " ");
+
+		/* then recur on left subtree */
+		printPreorder(root2.left);
+
+		/* now recur on right subtree */
+		printPreorder(root2.right);
+	}
+
+	// Wrappers over above recursive functions
+	void printPostorder() {
+		printPostorder(root);
 	}
 
 	void printInorder() {
-		printInorder(this.root);
-	}
-
-	private void printInorder(Node root2) {
-		if(root2 == null) {
-			return;
-		}
-		
-		printInorder(root2.left);
-		
-		System.out.print(root2.key + " ");
-		
-		printInorder(root2.right);
-		
-		
-		
+		printInorder(root);
 	}
 
 	void printPreorder() {
-		printPreorder(this.root);
-	}
-
-	private void printPreorder(Node root2) {
-		// TODO Auto-generated method stub
-		
+		printPreorder(root);
 	}
 }
 
@@ -67,7 +95,14 @@ public class BinaryTreeExample {
 		tree.root.left.left = new Node(4);
 		tree.root.left.right = new Node(5);
 
+		System.out.println("Preorder traversal of binary tree is ");
+		tree.printPreorder();
+
+		System.out.println("\nInorder traversal of binary tree is ");
 		tree.printInorder();
-		
+
+		System.out.println("\nPostorder traversal of binary tree is ");
+		tree.printPostorder();
+
 	}
 }
